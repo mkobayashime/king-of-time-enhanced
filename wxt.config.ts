ef74cf42d9ef31c3adb53b2163654071c2a65d85
Masaki Kobayashi <contact@mkobayashi.me>
2024-12-14 13:11:40 +0900

    Enable `src` dir

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "wxt";
 import { description, version } from "./package.json";
 
 export default defineConfig({
+  srcDir: "src",
   extensionApi: "chrome",
   manifest: {
     name: "KING OF TIME Enhanced",
