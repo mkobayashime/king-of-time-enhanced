6fc0f5dd5be9b4398f507f21ce1c9dce3a47306d
Masaki Kobayashi <contact@mkobayashi.me>
2024-12-14 13:35:11 +0900

    Disable auto imports

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,7 @@ import { description, version } from "./package.json";
 
 export default defineConfig({
   srcDir: "src",
+  imports: false,
   extensionApi: "chrome",
   manifest: {
     name: "KING OF TIME Enhanced",
