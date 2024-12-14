5bb9c53acf82a7c74017f370ab4ee4affbb98b91
Masaki Kobayashi <contact@mkobayashi.me>
2024-12-14 16:43:51 +0900

    Change visibility of buttons according to the current situation

---
+++ src/entrypoints/content/handleRecordButtonsVisibility/index.ts
@@ -0,0 +1,64 @@
+import { awaitWithInterval } from "../../../utils/awaitWithInterval";
+import type { Consts } from "../../../utils/consts";
+import { getRecordButtons } from "./getRecordButtoms";
+import { getTimecard } from "./getTimecard";
+import { parseTodaysTimes } from "./parseTodaysTimes";
+
+const overwriteStyle = () => {
+  const styleElement = document.createElement("style");
+  styleElement.innerHTML = `
+#buttons li {
+  display: none;
+}
+
+.record-btn-outer {
+  background: unset !important;
+}
+`;
+
+  document.body.append(styleElement);
+};
+
+export const handleRecordButtonsVisibility = async (consts: Consts) => {
+  const [timecardPage, dateString] = await Promise.all([
+    getTimecard(consts),
+    awaitWithInterval(() => {
+      const dateElement = document.querySelector(".watch-year-to-date");
+      if (!(dateElement instanceof HTMLElement)) return;
+
+      const [, dateString] =
+        /\d+年(\d+月\d+日)/.exec(dateElement.innerText) ?? [];
+
+      if (dateString) return { data: dateString };
+    }),
+  ]);
+  if (!timecardPage || !dateString) return;
+
+  const times = await parseTodaysTimes({ timecardPage, dateString });
+  if (!times) return;
+
+  const recordButtons = await getRecordButtons(consts);
+  if (!recordButtons) return;
+
+  if (times.start.length > times.end.length) {
+    if (times.startRest.length > times.endRest.length) {
+      /**
+       * 休憩中
+       */
+      recordButtons.buttonEndRest.style.display = "inline-block";
+    } else {
+      /**
+       * 休憩中でない
+       */
+      recordButtons.buttonEnd.style.display = "inline-block";
+      recordButtons.buttonStartRest.style.display = "inline-block";
+    }
+  } else {
+    /**
+     * 勤務中でない
+     */
+    recordButtons.buttonStart.style.display = "inline-block";
+  }
+
+  overwriteStyle();
+};
--- src/entrypoints/content/index.ts
+++ src/entrypoints/content/index.ts
@@ -1,4 +1,7 @@
+import * as v from "valibot";
 import { defineContentScript } from "wxt/sandbox";
+import { constsSchema } from "../../utils/consts";
+import { handleRecordButtonsVisibility } from "./handleRecordButtonsVisibility";
 import { redirectTimecardLink } from "./redirectTimecardLink";
 import { setModalShortcuts } from "./setModalShortcuts";
 import { setRoleToButtons } from "./setRoleToButtons";
@@ -6,6 +9,18 @@ import { setRoleToButtons } from "./setRoleToButtons";
 export default defineContentScript({
   matches: ["https://s2.kingtime.jp/independent/recorder2/personal/*"],
   main() {
+    document.addEventListener("consts", (e) => {
+      if (
+        v.is(
+          v.object({
+            detail: constsSchema,
+          }),
+          e,
+        )
+      ) {
+        void handleRecordButtonsVisibility(e.detail);
+      }
+    });
     void setRoleToButtons();
     void redirectTimecardLink();
     setModalShortcuts();
