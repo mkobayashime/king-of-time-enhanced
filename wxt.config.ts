2dd2188e891c5230f444a9f173252435d16abcdb
Masaki Kobayashi <contact@mkobayashi.me>
2024-12-14 12:54:33 +0900

    Set basic information of the extension

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "wxt";
+import { description, version } from "./package.json";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   extensionApi: "chrome",
+  manifest: {
+    name: "KING OF TIME Enhanced",
+    description,
+    version,
+  },
 });
