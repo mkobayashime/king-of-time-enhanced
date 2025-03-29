53d0ef5f56c1edb02615152ef5bc18ede399d80c
Masaki Kobayashi <contact@mkobayashi.me>
2025-03-29 15:13:38 +0900

    Migration for WXT v0.20.0

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,11 +4,11 @@ import { description, version } from "./package.json";
 export default defineConfig({
   srcDir: "src",
   imports: false,
-  extensionApi: "chrome",
   manifest: {
     name: "KING OF TIME Enhanced",
     description,
     version,
     host_permissions: ["https://*.kingtime.jp/*", "https://*.kingoftime.jp/*"],
   },
+  outDirTemplate: "{{browser}}-mv{{manifestVersion}}",
 });
